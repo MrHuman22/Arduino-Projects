1d8b8deb1ac719289be10a35260a3641e0b8da69
Pete Newman <pwn394@newcastle.edu.au>
2020-07-10 14:25:55 +1000

    300ms between commands is the fastest we can manage

--- ServoTest/ServoTest.ino
+++ ServoTest/ServoTest.ino
@@ -1,9 +1,37 @@
+#include <Servo.h>
+
+const int servoPin = 9;
+const int potPin = A1;
+const int restPoint = 60;
+const int endPoint = 150;
+const int startPoint = 0;
+Servo myServo;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  myServo.attach(9);
+
+  // check the extremes
+  myServo.write(startPoint);
+//  delay(1000);
+//  myServo.write(endPoint);
+//  delay(1000);
+//  myServo.write(restPoint);
+//  delay(1000);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int potVal = analogRead(potPin);
+  int interval = map(potVal,0,1023,10,2000);
+  Serial.print("potVal: ");
+  Serial.print(potVal);
+  Serial.print(" interval: ");
+  Serial.println(interval);
+  myServo.write(endPoint);
+  delay(interval);
+  myServo.write(restPoint);
+  delay(interval);
 }
