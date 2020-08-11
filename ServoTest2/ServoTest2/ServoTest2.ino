9fba0da5222f5d655b0430341856441f64932bf6
Pete Newman <pwn394@newcastle.edu.au>
2020-08-11 16:37:58 +1000

    Outlining servo tests

--- ServoTest2/ServoTest2/ServoTest2.ino
+++ ServoTest2/ServoTest2/ServoTest2.ino
@@ -1,3 +1,38 @@
+/*
+
+Servo Test
+
+Problem: how do we program arduinos with out programming arduinos?
+Answer: make them adjustable
+
+What are we adjusting?
+- Range of motion
+- Gap between instructions
+
+How do we do this with one pot per servo?
+- Switch: one direction controls range of motion, the other the timing
+
+Test 1:
+- Adjust range of motion
+
+Test 2:
+- Adjust timing
+
+Test 3:
+- Switching modes using toggle switch
+
+Test 4:
+- Bringing it all together
+
+Author: Peter Newman
+Organisation: University of Newcastle Science and Engineering Challenge
+*/
+
+#include <Servo.h>
+
+const int pot = A0;
+const int servoPin = 3;
+
 void setup() {
   // put your setup code here, to run once:
 
