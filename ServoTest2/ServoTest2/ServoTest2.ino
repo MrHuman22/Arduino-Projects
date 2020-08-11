b76c56865c5e71333fe1f16230fce302b920eca6
Pete Newman <pwn394@newcastle.edu.au>
2020-08-11 16:45:57 +1000

    init statement done. Trouble uploading to board on work laptop.

--- ServoTest2/ServoTest2/ServoTest2.ino
+++ ServoTest2/ServoTest2/ServoTest2.ino
@@ -1,41 +1,55 @@
 /*
 
-Servo Test
+  Servo Test
 
-Problem: how do we program arduinos with out programming arduinos?
-Answer: make them adjustable
+  Problem: how do we program arduinos with out programming arduinos?
+  Answer: make them adjustable
 
-What are we adjusting?
-- Range of motion
-- Gap between instructions
+  What are we adjusting?
+  - Range of motion
+  - Gap between instructions
 
-How do we do this with one pot per servo?
-- Switch: one direction controls range of motion, the other the timing
+  How do we do this with one pot per servo?
+  - Switch: one direction controls range of motion, the other the timing
 
-Test 1:
-- Adjust range of motion
+  Test 1:
+  - Adjust range of motion
 
-Test 2:
-- Adjust timing
+  Test 2:
+  - Adjust timing
 
-Test 3:
-- Switching modes using toggle switch
+  Test 3:
+  - Switching modes using toggle switch
 
-Test 4:
-- Bringing it all together
+  Test 4:
+  - Bringing it all together
 
-Author: Peter Newman
-Organisation: University of Newcastle Science and Engineering Challenge
+  Author: Peter Newman
+  Organisation: University of Newcastle Science and Engineering Challenge
 */
 
 #include <Servo.h>
 
+//pin variables
 const int pot = A0;
 const int servoPin = 3;
+const int ledPin = 13;
+
+Servo myServo;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  myServo.attach(servoPin);
+
+  // testing the servo;
+  myServo.write(0);
+  delay(1000);
+  myServo.write(180);
+  delay(1000);
+  myServo.write(90);
+  delay(1000);
 }
 
 void loop() {
